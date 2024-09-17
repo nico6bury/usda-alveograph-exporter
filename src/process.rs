df4d96beaa8dfb82818f2e35ec728a237aac2057
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2024-09-17 13:46:14 -0500

    Added a bunch of styling to make the output files match the example just about exactly.

--- src/process.rs
+++ src/process.rs
@@ -1,6 +1,18 @@
 use std::path::PathBuf;
-use rust_xlsxwriter::{Format, FormatAlign, Workbook, XlsxError};
+use rust_xlsxwriter::{Format, FormatAlign, FormatBorder, Workbook, XlsxError};
 use crate::data::Data;
+
+/// The border style to use for all the cells we write to.
+const BORDER_FORMAT: FormatBorder = FormatBorder::Thin;
+/// The font size for cells in the header row.
+const FONT_SIZE_HEADER: i32 = 14;
+/// The font size for cells with test name label.
+const FONT_SIZE_TEST_NAME: i32 = 11;
+/// The font size for cells with numeric data in them.
+const FONT_SIZE_DATA: i32 = 11;
+/// The row upon which the header starts.
+/// This acts as a vertical offset for the whole output.
+const HEADER_START_ROW: u32 = 2;
 
 /// Creates an excel workbook, which can then be used in
 /// further funtions.
@@ -23,16 +35,26 @@ pub fn write_output_to_sheet(workbook: &mut Workbook, data: &Vec<Data>, sheet_na
     if data.len() < 1 {return Ok(());}
 
     // write the header row
-    let bold = Format::new().set_bold().set_align(FormatAlign::Center);
-    sheet.write_with_format(0,0,"test-name", &bold)?;
+    let bold = Format::new()
+        .set_bold()
+        .set_align(FormatAlign::Center)
+        .set_border(BORDER_FORMAT)
+        .set_font_size(FONT_SIZE_HEADER);
+    sheet.write_with_format(HEADER_START_ROW,0,"Test Name", &bold)?;
     for (index, row) in data.first().expect("already checked").row_data.iter().enumerate() {
         let index = index as u16;
-        sheet.write_with_format(0,index + 1, row.header.clone(),&bold)?;
+        sheet.write_with_format(HEADER_START_ROW,index + 1, row.header.clone(),&bold)?;
     }//end writing each row header
 
-    let test_name_format = Format::new().set_align(FormatAlign::Center);
-    let default_format = Format::new().set_align(FormatAlign::Center);//.set_num_format("0.00");
-    let mut row_num = 1;
+    let test_name_format = Format::new()
+        .set_align(FormatAlign::Center)
+        .set_border(BORDER_FORMAT)
+        .set_font_size(FONT_SIZE_TEST_NAME);
+    let default_format = Format::new()
+        .set_align(FormatAlign::Center)
+        .set_border(BORDER_FORMAT)
+        .set_font_size(FONT_SIZE_DATA);
+    let mut row_num = HEADER_START_ROW + 1;
     for data_file in data {
         sheet.write_with_format(row_num,0,data_file.test_name.clone(), &test_name_format)?;
         for (col_offset,row) in data_file.row_data.iter().enumerate() {
@@ -42,7 +64,7 @@ pub fn write_output_to_sheet(workbook: &mut Workbook, data: &Vec<Data>, sheet_na
         row_num += 1;
     }//end looping over each data file
 
-    sheet.set_column_width(0, 20)?;
+    sheet.set_column_width(0, 14.5)?;
 
     Ok(())
 }//end write_output_to_sheet()
